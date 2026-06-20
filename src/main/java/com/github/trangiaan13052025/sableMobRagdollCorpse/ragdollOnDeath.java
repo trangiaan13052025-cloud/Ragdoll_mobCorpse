71378242f36869fb55af98934b9fe0fc76b06243
Andy <Trangiaan13052025@gmail.com>
2026-06-20 21:58:42 +0700

    dependency

--- src/main/java/com/github/trangiaan13052025/sableMobRagdollCorpse/ragdollOnDeath.java
+++ src/main/java/com/github/trangiaan13052025/sableMobRagdollCorpse/ragdollOnDeath.java
@@ -7,9 +7,11 @@ import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Mob;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.Vec3;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.event.entity.EntityJoinLevelEvent;
 import net.neoforged.neoforge.event.entity.living.LivingDamageEvent;
 import net.neoforged.neoforge.event.entity.living.LivingDeathEvent;
 import net.neoforged.neoforge.event.tick.EntityTickEvent;
@@ -48,27 +50,7 @@ public class ragdollOnDeath {
                 Vec3 curDelta = mob.getDeltaMovement();
                 float baseForce = damage*10;
                 Vec3 delta = new Vec3(curDelta.x * baseForce, curDelta.y * baseForce + (baseForce/100), curDelta.z * baseForce);
-                System.out.println(delta);
                 double spinFactor = baseForce;
-
-                //EntityType<?> type = mob.getType();
-//                if (source.getSourcePosition() != null) {
-//                    Vec3 sourcePos = source.getSourcePosition();
-//                    Vec3 launchDirection = mob.position().subtract(sourcePos).normalize();
-//                    double force = source.is(net.minecraft.tags.DamageTypeTags.IS_EXPLOSION) ? 1.2 : 0.4;
-//                    delta = delta.add(new Vec3(launchDirection.x * force, 0.35, launchDirection.z * force));
-//                }
-//                else if (source.getEntity() instanceof LivingEntity attacker) {
-//                    // ⚔️ MELEE ATTACKS: If no exact position exists but an attacker does, fly away from attacker
-//                    Vec3 launchDirection = mob.position().subtract(attacker.position()).normalize();
-//                    delta = new Vec3(launchDirection.x * 0.4, 0.25, launchDirection.z * 0.4);
-//                }
-//                else {
-//                    // 🍂 NATURAL DEATHS (Fall damage, Poison, Void, Suffocation)
-//                    // Let it drop down naturally, but give it a tiny random twitch so it doesn't look stiff
-//                    delta = new Vec3((Math.random() - 0.5) * 0.1, -0.1, (Math.random() - 0.5) * 0.1);
-//                    spinFactor = 0.2; // Minor slump twist
-//                }
 
                 double spinX = (Math.random() - 0.5) * spinFactor;
                 double spinY = (Math.random() - 0.5) * spinFactor;
@@ -112,8 +94,39 @@ public class ragdollOnDeath {
                 } else {
                     // Increment the counter by 1 every game tick and save it back onto the mob
                     mob.setData(EntityAttachments.corpsetimer, currentTicks - 1);
+                    ServerLevel level = (ServerLevel) mob.level();
+                    if (!RagdollAPI.isMobRagdolled(mob)) {
+                        RagdollAPI.launchMob(
+                                level,
+                                mob,
+                                new Vec3(0, 0, 0),
+                                new Vec3(0, 0, 0),
+                                MobRagdollLaunchOptions.builder().durationTicks(999999999).build()
+                        );
+                    }
                 }
             }
         }
     }
+
+    @SubscribeEvent
+    public static void onMobLoad(EntityJoinLevelEvent event) {
+        Entity entity = event.getEntity();
+        if (entity.level().isClientSide()) {
+            return;
+        }
+        if (entity instanceof Mob mob) {
+            if (!mob.hasData(EntityAttachments.is_ragdoll_corpse)) {
+                return;
+            }
+
+            if (RagdollAPI.isMobRagdolled(mob)) {
+                return;
+            }
+
+            mob.setHealth(0);
+            mob.deathTime = 20;
+        }
+    }
+
 }
